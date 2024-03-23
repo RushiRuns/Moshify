be40ccb76a48603f096a5fa812019daa4b719c85
rushikesh_aundhakar <rushikeshaundhakar715@gmail.com>
2024-03-23 14:02:17 +0530

    Build navbar component

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,20 @@
-const collapsible = document.querySelectorAll(".collapsible");
-collapsible.forEach((item) => {
-  item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expand");
+const collapsible_btn = document.querySelectorAll(".collapsible__btn");
+
+collapsible_btn.forEach((button) => {
+  button.addEventListener("click", function () {
+    this.closest(".nav").classList.toggle("collapsible--expand");
   });
 });
+
+// const collapsible = document.querySelector(".collapsible");
+// collapsible.addEventListener("click", function () {
+//   this.classList.toggle("collapsible--expand");
+// });
+
+// const collapsible = document.querySelector(".collapsible");
+
+// collapsible_btn.addEventListener("click", expand);
+
+// function expand() {
+//   collapsible.classList.toggle("collapsible--expand");
+// }
