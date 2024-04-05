3e420c6bd69f56b6cbbed4f9f4cb9e4bbcf5dbbf
rushikesh_aundhakar <rushikeshaundhakar715@gmail.com>
2024-04-05 12:55:05 +0530

    Build footer block

--- JS/main.js
+++ JS/main.js
@@ -2,9 +2,17 @@ const collapsible_btn = document.querySelectorAll(".collapsible__btn");
 
 collapsible_btn.forEach((button) => {
   button.addEventListener("click", function () {
-    this.closest(".nav").classList.toggle("collapsible--expand");
+    this.closest(".collapsible").classList.toggle("collapsible--expand");
   });
 });
+
+// const collapsible_btn = document.querySelectorAll(".collapsible__btn");
+
+// collapsible_btn.forEach((button) => {
+//   button.addEventListener("click", function () {
+//     this.closest(".nav").classList.toggle("collapsible--expand");
+//   });
+// });
 
 // const collapsible = document.querySelector(".collapsible");
 // collapsible.addEventListener("click", function () {
